671d34384cc8fd648d8e8699cae55b1992a818f4
Andrea Alheli Gonzalez <11069744@uvu.edu>
2024-05-24 18:12:10 -0600

    Update SwitchesChallenge.cs

--- SwitchesChallenge.cs
+++ SwitchesChallenge.cs
@@ -1,4 +1,59 @@
-// The Expectation is that you write your own code based
-// on what you have learned in the class, it can be what you want
-// If you are havind trouble thinging of something, write conditionals
-// with the topic of PowerUps, what happens if the powerUp is good or bad.
+using System;
+					
+public class Menu
+{
+  public static void Main()
+  {
+    Combo combo1 = new Combo();
+    Combo combo2 = new Combo();
+    Combo combo3 = new Combo();
+    
+    combo1.entree = "Hamburger";
+    combo1.side = "Fries";
+    combo1.drink = "Dr Pepper";
+    
+    combo2.entree = "Hamburger";
+    combo2.side = "Apple Slices";
+    combo2.drink = "Coke";
+    
+    combo3.entree = "Hotdog";
+    combo3.side = "Fries";
+    combo3.drink = "Dr Pepper";
+    
+    Console.WriteLine("Combo #1 Entree: " + combo1.entree);
+	Console.WriteLine("Combo #1 Side: " + combo1.side);
+    Console.WriteLine("Combo #1 Drink: " + combo1.drink);
+    
+    Console.WriteLine("Combo #2 Entree: " + combo2.entree);
+	Console.WriteLine("Combo #2 Side: " + combo2.side);
+    Console.WriteLine("Combo #2 Drink: " + combo2.drink);
+    
+    Console.WriteLine("Combo #3 Entree: " + combo3.entree);
+	Console.WriteLine("Combo #3 Side: " + combo3.side);
+    Console.WriteLine("Combo #3 Drink: " + combo3.drink);
+    
+    Price();
+ }
+  
+	public static void Price()
+	{
+		Console.WriteLine("Menu Combos Prices");
+		Console.WriteLine("Combo 1 Price:" + DoMath(4, 3));
+        Console.WriteLine("Combo 2 Price:" + DoMath(4, 2));
+        Console.WriteLine("Combo 3 Price:" + DoMath(3, 3));
+	}
+	
+	public static void DoMath (int value, int value2) 
+    {
+		var number = value + value2;
+		Console.WriteLine(number);
+        
+        }
+	}
+    
+public class Combo
+ {
+    public string entree;
+    public string side;
+    public string drink;
+ }
